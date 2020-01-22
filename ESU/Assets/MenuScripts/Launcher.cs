d24203f947e509aa53d06c9fd0b0e40b72fb174d
Maxence Oden <maxence.oden@epita.fr>
2020-01-22 18:20:11 +0100

    :sparkles: Add Creation Room script

---
+++ ESU/Assets/MenuScripts/Launcher.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.DeltaPlane.ESU
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        #region Private Serializable Fields
+
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 6;
+
+        #endregion
+
+        #region Private Fields
+
+        private string gameVersion = "1";
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        void Awake()
+        {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        void Start()
+        {
+            Connect();
+        }
+        #endregion
+
+        #region Public Methods
+
+        public void Connect()
+        {
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.IsConnected)
+            {
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+
+        public void CreateRoom()
+        {
+            Debug.Log("Create a new room");
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+        }
+
+        #endregion
+        
+        #region MonoBehaviourPunCallbacks Callbacks
+
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Basics Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Basics Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+
+        #endregion
+    }
+
+}
