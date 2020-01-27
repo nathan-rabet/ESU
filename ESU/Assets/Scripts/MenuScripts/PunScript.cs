2c3bd8a2f2bacbb4a4e97364bef904bbfe7f1767
Maxence Oden <maxence.oden@epita.fr>
2020-01-27 20:22:16 +0100

    :sparkles: Add Player Prefab

---
+++ ESU/Assets/Scripts/MenuScripts/PunScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace ESU.Deltaplane {
+    public class PunScript : MonoBehaviourPunCallbacks
+    {
+        public Text TxtInfosPun;
+        public Transform SpawnPoint;
+        public GameObject PrefabPlayer;
+
+            void Start()
+            {
+                    PhotonNetwork.ConnectUsingSettings();
+            }
+
+            public override void OnConnectedToMaster()
+            {
+                PhotonNetwork.JoinLobby (TypedLobby.Default);
+            }
+
+
+            public override void OnJoinedLobby()
+            {
+                    RoomOptions myRoomOption = new RoomOptions();
+                    myRoomOption.MaxPlayers = 20;
+                    
+                    PhotonNetwork.JoinOrCreateRoom ("OfficialRoom", myRoomOption, TypedLobby.Default);
+            }
+
+            public override void OnJoinedRoom () 
+            {
+                PhotonNetwork.Instantiate(PrefabPlayer.name,SpawnPoint.position,Quaternion.identity,0);
+            }
+
+            public void LeaveTheRoom()
+            {
+                PhotonNetwork.LeaveRoom();
+            }
+            public override void OnLeftRoom () 
+            {
+                SceneManager.LoadScene(0);
+            }
+        // Update is called once per frame
+        void Update()
+        {
+            TxtInfosPun.text = PhotonNetwork.NetworkClientState.ToString();
+        }
+    }
+}
---
+++ ESU/Assets/Scripts/PlayersScripts/PlayerMouvement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PhotonView;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public int speed = 5;
+    PhotonView view;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (view.isMine)
+        {
+            transform.Translate (Vector3.forward * Input.GetAxis ("Vertical") * speed * Time.deltaTime);
+            transform.Rotate (Vector3.up * Input.GetAxis("Horizontal") * speed * 40 * Time.deltaTime);
+        }
+    }
+}
