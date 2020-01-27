9b4dfc339e6895bb9683c7c1d9b1b63659811963
Maxence Oden <maxence.oden@epita.fr>
2020-01-27 15:53:45 +0100

    :sparkles: Add Connection System

---
+++ ESU/Assets/MenuScripts/PunScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PunScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text TxtInfosPun;
+        void Start()
+        {
+                PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public void ConnectToRoom()
+        {
+                RoomOptions myRoomOption = new RoomOptions();
+                myRoomOption.MaxPlayers = 20;
+                
+                PhotonNetwork.JoinOrCreateRoom ("OfficialRoom", myRoomOption, TypedLobby.Default);
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TxtInfosPun.text = PhotonNetwork.NetworkClientState.ToString();
+    }
+}
