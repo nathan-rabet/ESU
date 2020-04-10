b7afab31252d836230eef3ebd3513418403e34d2
Maxence Oden <maxence.oden@epita.fr>
2020-04-10 14:49:04 +0200

    :wrench: Improving player mouvement script

--- ESU/Assets/Scripts/PlayersScripts/PlayerMouvement.cs
+++ ESU/Assets/Scripts/PlayersScripts/PlayerMouvement.cs
@@ -10,6 +10,7 @@ public class PlayerMouvement : MonoBehaviour
     
     private Animator anim;
     private float speed = 0.08f;
+    private float maxspeed = 0.16f;
     private float jumpHight = 10.0f;
     private bool canJump = true;
     public GameObject mainCamera;
@@ -52,11 +53,11 @@ public class PlayerMouvement : MonoBehaviour
 
             if (Input.GetKey(KeyCode.LeftShift)) //Si Maj enfoncé
             {
-                speed = 0.15f; //vitesse
+                speed = maxspeed; //vitesse
             }
             else
             {
-                speed = 0.08f; //vitesse
+                speed = maxspeed/2; //vitesse
             }
             //Création du vecteur direction du mouvement
             Vector3 dirVector = (Input.GetAxis("Horizontal") * MyRigidBody.transform.right + Input.GetAxis("Vertical") * MyRigidBody.transform.forward).normalized;
