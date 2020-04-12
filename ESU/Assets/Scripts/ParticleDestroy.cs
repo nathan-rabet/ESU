36cdb6495cefec17f773432661ec5290930870ae
Maxence Oden <maxence.oden@epita.fr>
2020-04-12 13:06:29 +0200

    :sparkles: Add Pistol particules

---
+++ ESU/Assets/Scripts/ParticleDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroy : MonoBehaviour
+{
+    public GameObject particle;
+    public int minDuration;
+    public int maxDuration;
+    private float duration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        duration = Random.Range(minDuration, maxDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (duration > 0)
+        {
+            duration -= Time.deltaTime;
+        }
+        else
+            Destroy(particle);
+    }
+}
