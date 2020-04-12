f80f0aa8c0252413338f5169f5d15dd13d402e8f
Louis Place <placelouis@gmail.com>
2020-04-12 18:16:33 +0200

    :sparkles: [Map 2] Adding Clouds and new details as Volcano

---
+++ ESU/Assets/Assets/Environnements/LowPolyFantasyClouds/Script/MouveCloud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouveCloud : MonoBehaviour
+{
+    public float speed = 0.2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float zCoord = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(0,0,speed * Time.deltaTime);
+    }
+}
