62fd2ff8cb10254922ca810a80360371f1ece046
Maxence Oden <maxence.oden@epita.fr>
2020-04-17 21:46:25 +0200

    :sparkles: New option UI

---
+++ ESU/Assets/Scripts/MenuScripts/GameSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameSettings : MonoBehaviour
+{
+    public AudioMixer MusicAM;
+    public AudioMixer GameAM;
+    public void SetMusicVolume (float volume)
+    {
+        MusicAM.SetFloat("MusicVolume", volume);
+    }
+    public void SetGameVolume (float volume)
+    {
+        GameAM.SetFloat("GameVolume", volume);
+    }
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
