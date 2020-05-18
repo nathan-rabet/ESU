af930f3cd422aabd257490ac0df40b2408c180da
Louis Place <placelouis@gmail.com>
2020-05-18 22:45:37 +0200

    :sparkles: [INVENTORY] First version of Inventory System *Need Test in Multiplayer*

--- ESU/Assets/Assets/PowerUp/Script/PowerUp.cs
+++ ESU/Assets/Assets/PowerUp/Script/PowerUp.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+﻿using Boo.Lang.Environments;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUp : MonoBehaviour {
 
     public GameObject pickupEffect;
+    public Text myText;
+    public Image slot0;
+    public Image slot1;
+    public Image slot2;
+    public Image slot3;
+    public Sprite icon;
+    //Choose effect
+    public bool Heal;
+    public bool Ammo;
+    public bool Shield;
+    public bool Speed;
+    public bool Jump;
+
     public float duration = 4f;
     public float respawn = 10f;
-    public float multiplierPlayerScale = 1f;
     public float multiplierSpeed = 1.4f;
     public float multiplierJump = 1.4f;
+
+    private bool toActivate = false;
+    private Collider playerPick = null;
+    private Inventory inventory;
     private ParticleSystem ps;
+    private int slot;
+    
+
 
     void OnTriggerEnter (Collider other) 
     {
@@ -19,62 +41,168 @@ public class PowerUp : MonoBehaviour {
 
         if (other.CompareTag("Player"))
         {
+            inventory = other.GetComponent<Inventory>();
+            PlayerMouvement mouv = other.GetComponent<PlayerMouvement>();
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    // Item can be added to Inventory !
+                    inventory.isFull[i] = true;
+                    Pickup(other, mouv, i);
+                    StartCoroutine(displayText());
+                    StartCoroutine(RespawnPower());
+                    toActivate = true;
+                    playerPick = other;
+                    slot = i;
+                    break;
+                }
+            }
 
-            StartCoroutine( Pickup(other) );
-            StartCoroutine(ActivePower());
         }
     }
 
-    IEnumerator Pickup(Collider player) 
+    public void Update()
     {
-        bool isJumpModif = false;
-        bool isSpeedModif = false;
-        bool isScaleModif = false;
+        switch (slot)
+        {
+            case 0:
+                toLaunchActivePower(KeyCode.W);
+                break;
+            case 1:
+                toLaunchActivePower(KeyCode.X);
+                break;
+            case 2:
+                toLaunchActivePower(KeyCode.C);
+                break;
+            case 3:
+                toLaunchActivePower(KeyCode.V);
+                break;
+        }
+        
+    }
+
+    void Pickup(Collider player, PlayerMouvement mouv, int i) 
+    {
 
         //Spawn a cool effect
         Instantiate(pickupEffect, transform.position, transform.rotation);
         
-
-        //Apply an effect to the player
-        if (multiplierPlayerScale > 0)
-        {
-            player.transform.localScale *= multiplierPlayerScale;
-            isScaleModif = true;
-        }
-        PlayerMouvement mouv = player.GetComponent<PlayerMouvement>();
-        if (multiplierSpeed > 0)
-        {
-            mouv.MaxSpeed *= multiplierSpeed;
-            isSpeedModif = true;
-        }
-        if (multiplierJump > 0)
-        {
-            mouv.JumpHight *= multiplierJump;
-            isJumpModif = true;
-        }
         var emission = ps.emission;
         emission.enabled = false;
         GetComponent<Collider>().enabled = false;
+        switch (i)
+        {
+            case 0:
+                slot0.enabled = true;
+                slot0.sprite = icon;
+                break;
+            case 1:
+                slot1.enabled = true;
+                slot1.sprite = icon;
+                break;
+            case 2:
+                slot2.enabled = true;
+                slot2.sprite = icon;
+                break;
+            case 3:
+                slot3.enabled = true;
+                slot3.sprite = icon;
+                break;
+        }
+
         
 
-        yield return new WaitForSeconds(duration);
-        
-        if (isJumpModif)
-            mouv.JumpHight /= multiplierJump;
-        if (isSpeedModif)
-            mouv.MaxSpeed /= multiplierSpeed;
-        if (isScaleModif)
-            player.transform.localScale /= multiplierPlayerScale;
 
     }
-    IEnumerator ActivePower() 
+    public IEnumerator ActivePower(Collider player, PlayerMouvement mouv, int i) 
     {
-        yield return new WaitForSeconds(respawn+duration);
+
+
+        //Consumme PowerUP
+        inventory.isFull[i] = false;
+        switch (i)
+        {
+            case 0:
+                slot0.enabled = false;
+                break;
+            case 1:
+                slot1.enabled = false;
+                break;
+            case 2:
+                slot2.enabled = false;
+                break;
+            case 3:
+                slot3.enabled = false;
+                break;
+        }
+        //Apply an effect to the player
+
+        //Heal
+        if (Heal)
+        {
+            Player_Manager manager = player.GetComponent<Player_Manager>();
+            manager.healing(manager.view.ViewID, 100);
+        }
+        //Ammo
+        if (Ammo) 
+        {
+            AmmoCount ammoCount;
+            ammoCount = GameObject.Find("/GAME/Menu/InGameHUD/Weapon Info").GetComponent<AmmoCount>();
+            ammoCount.SetAmmo(20);
+        }
+        //Shield
+        if (Shield)
+        {
+            Player_Manager manager = player.GetComponent<Player_Manager>();
+            manager.isShieldActive = true;
+        }
+
+        //Speed
+        if (Speed)
+            mouv.MaxSpeed *= multiplierSpeed;
+        
+
+        //Jump
+        if (Jump)
+            mouv.JumpHight *= multiplierJump;
+
+
+        yield return new WaitForSeconds(duration);
+
+        if (Jump)
+            mouv.JumpHight /= multiplierJump;
+
+        if (Speed)
+            mouv.MaxSpeed /= multiplierSpeed;
+
+       
+
+    }
+    IEnumerator RespawnPower() 
+    {
+        yield return new WaitForSeconds(respawn);
         var emission = ps.emission;
         emission.enabled = true;
         GetComponent<Collider>().enabled = true;
        
 
     }
-   
+
+    IEnumerator displayText()
+    {
+        myText.text = "You picked a "+icon.name+"!!";
+        yield return new WaitForSeconds(5);
+        myText.text = "";
+    }
+    void toLaunchActivePower(KeyCode code)
+    {
+        if (Input.GetKeyUp(code) && toActivate)
+        {
+            PlayerMouvement mouv = playerPick.GetComponent<PlayerMouvement>();
+            StartCoroutine(ActivePower(playerPick, mouv, slot));
+            toActivate = false;
+        }
+    }
+
 }
---
+++ ESU/Assets/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+
+    //AMMO
+    
+    //SHIELD
+    
+    //SPEED
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
