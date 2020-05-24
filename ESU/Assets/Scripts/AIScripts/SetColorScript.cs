30d5e62441955b772ade1c23a92a5219b3470c26
Maxence Oden <maxence.oden@epita.fr>
2020-05-24 13:14:59 +0200

    Add audio

--- ESU/Assets/Scripts/AIScripts/SetColorScript.cs
+++ ESU/Assets/Scripts/AIScripts/SetColorScript.cs
@@ -4,31 +4,27 @@ using UnityEngine;
 
 public class SetColorScript : MonoBehaviour
 {
-    private static List<Color> colors = new List<Color> 
-    { 
+    private static List<Color> colors = new List<Color>
+    {
         Color.black,
-        Color.blue,
         Color.cyan,
         Color.gray,
-        Color.green,
-        Color.grey,
-        Color.magenta,
-        Color.red,
         Color.white,
-        Color.yellow
+        Color.yellow,
+        new Color(0.8f,1f,0.7568f),
+        new Color(0.6117f,0.6392f,1f),
+        new Color(0.949f,0.6705f,1f),
+        new Color(1f,0.5529f,0.5529f)
     };
 
     public static void SetColor(GameObject character)
     {
         Renderer ren = character.GetComponent<Renderer>();
-        string t = "";
-        foreach (Material mat in ren.materials)
-            t += mat.name + " ";
-        Debug.Log(t);
-        ren.materials[2].color = colors[Mathf.FloorToInt(Random.Range(0, 9.9f))];
-        ren.materials[4].color = colors[Mathf.FloorToInt(Random.Range(0, 9.9f))];
-        ren.materials[6].color = colors[Mathf.FloorToInt(Random.Range(0, 9.9f))];
-        ren.materials[7].color = colors[Mathf.FloorToInt(Random.Range(0, 9.9f))];
-        ren.materials[8].color = colors[Mathf.FloorToInt(Random.Range(0, 9.9f))];
+        float randUp = colors.Count - 0.1f;
+        ren.materials[2].color = colors[Mathf.FloorToInt(Random.Range(0, randUp))];
+        ren.materials[4].color = colors[Mathf.FloorToInt(Random.Range(0, randUp))];
+        ren.materials[6].color = colors[Mathf.FloorToInt(Random.Range(0, randUp))];
+        ren.materials[7].color = colors[Mathf.FloorToInt(Random.Range(0, randUp))];
+        ren.materials[8].color = colors[Mathf.FloorToInt(Random.Range(0, randUp))];
     }
 }
